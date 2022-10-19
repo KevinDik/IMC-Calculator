cdb1018199aa86e64af9d5f8c585d331d5f6293d
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-10-19 11:52:14 -0300

    04_Lógica de compartilhamento
    Feito uso da Api de compartilhamento

--- src/components/ResultImc/index.js
+++ src/components/ResultImc/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { View, Text} from "react-native"
+import { View, Text, TouchableOpacity, Share} from "react-native"
 import styles from './style'
 
 export default function ResultImc(props) {
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu Imc de hoje é: " + props.resultImc
+        })
+    }
+
     return(
             <View style={styles.ResultImc}>
+                <View style={styles.boxShareButton}>
+                    {props.resultImc != null ?
+                    <TouchableOpacity style={styles.share}
+                    onPress={onShare}>
+                        <Text style={styles.shareText}>Share</Text>
+                    </TouchableOpacity>
+                    :
+                    <View/>
+                    }
+                </View>
                 <Text style={styles.information}>{props.messsageResultImc}</Text>
                 <Text style={styles.numberImc}>{props.resultImc}</Text> 
             </View>
